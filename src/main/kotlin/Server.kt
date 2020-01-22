5a36bd0b4ba7b02e120441c8c3e24a550b5e5ec7
Calvin Marcoux <calvin.m.marcoux@gmail.com>
2020-01-22 08:51:24 -0500

    Add velocity

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -4,11 +4,19 @@ import com.tsuchigumo.crawl.HTMLExtraction
 import io.javalin.Javalin
 import io.javalin.core.compression.Brotli
 import io.javalin.core.compression.Gzip
+import io.javalin.plugin.rendering.JavalinRenderer
+import io.javalin.plugin.rendering.template.JavalinVelocity
 
 fun main(args: Array<String>) {
-    val app = Javalin.create{ config ->
+    val app = Javalin.create { config ->
         config.compressionStrategy(Brotli(4), Gzip(6))
+        JavalinRenderer.register(JavalinVelocity, ".vtl")
     }.start(7000)
-    app.get("/") { ctx -> ctx.result("Hello World") }
-    app.get("/api/crawl") { ctx -> ctx.json(HTMLExtraction().extract("https://eleveurs.ca"))}
+    app.get("/") { ctx ->
+        ctx.render(
+            "template/templateFile.vtl",
+            mapOf("data" to HTMLExtraction().extract("https://eleveurs.ca"))
+        ).contentType("text/javascript")
+    }
+    app.get("/api/crawl") { ctx -> ctx.json(HTMLExtraction().extract("https://eleveurs.ca")) }
 }
