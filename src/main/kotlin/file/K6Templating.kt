aadd3204981b5c130afa0005f1cd865ce45bdf9a
Calvin Marcoux <calvin.m.marcoux@gmail.com>
2020-01-23 18:38:39 -0500

    Add k6Options

--- src/main/kotlin/file/K6Templating.kt
+++ src/main/kotlin/file/K6Templating.kt
@@ -3,8 +3,25 @@ package com.tsuchigumo.file
 import com.tsuchigumo.crawl.BasePage
 import com.tsuchigumo.crawl.HTMLExtraction
 
+data class K6Stages(val duration: String, val target: Int)
+
+data class K6Options(
+        val stages: List<K6Stages>?,
+        val rps: Int?,
+        val noVUConnectionReuse: Boolean?,
+        val noConnectionReuse: Boolean?,
+        val maxRedirects: Int?,
+        val batch: Int?,
+        val batchPerHost: Int?,
+        val iterations: Int?,
+        val duration: Int?)
+
 object K6Templating {
     fun generateFile(params: Map<String, Any>): List<BasePage> {
+        return HTMLExtraction.extract(params)
+    }
+
+    fun generateFile(params: Map<String, Any>, options: K6Options): List<BasePage> {
         return HTMLExtraction.extract(params)
     }
 }
